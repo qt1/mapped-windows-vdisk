98e8fc6466242eec28a79d4eb8dc385ea591585c
baruch <baruch@ibn-labs.com>
2019-12-06 22:16:45 +0200

    fix (bypass) an error in template substitution

--- create_mapped_disk.py
+++ create_mapped_disk.py
@@ -7,14 +7,19 @@ import subprocess
 
 start_part_file = 'mbr_and_3partitions.raw'
 end_part_file = 'gpt2.raw'
-target_device_name='virtual_windows_disk'
+target_device_name = 'virtual_windows_disk'
+loop1_template = 'dev_loop1_template'
+loop2_template = 'dev_loop2_template'
+def_file_template_name = 'virtual_windows_disk_def'
+def_file_name = def_file_template_name+'_current'
 
-loop1 = subprocess.check_output(['losetup','--find','--show', start_part_file]).decode('utf-8')
-loop2 = subprocess.check_output(['losetup','--find','--show', end_part_file]).decode('utf-8')
-loop1_template='/dev/loop15'
-loop2_template='/dev/loop16'
-def_file_template_name='virtual_windows_disk_def'
-def_file_name=def_file_template_name+'_current'
+# TBD: Verify the disk structure is not changed - the disk and partitions IDs and sizes 
+#      of the config match the physical disk 
+
+loop1 = subprocess.check_output(['losetup', '--find', '--show', start_part_file]).decode('utf-8')
+loop2 = subprocess.check_output(['losetup', '--find', '--show', end_part_file]).decode('utf-8')
+loop1 = loop1.replace('\n', '')
+loop2 = loop2.replace('\n', '')
 
 # verify we got good loop names
 
@@ -24,23 +29,27 @@ if(not loop1.startswith('/dev/loop')):
 print(f'loop1 = {loop1}')
 
 if(not loop2.startswith('/dev/loop')):
-    subprocess.check_output(['losetup','--delete', loop1])
-    print("Error creating loop2. "+loop1)
+    subprocess.check_output(['losetup', '--delete', loop1])
+    print("Error creating loop2. "+loop2)
     exit(1)
 print(f'loop2 = {loop2}')
 
-with open(def_file_template_name,'r') as tf:
+with open(def_file_template_name, 'r') as tf:
     template = tf.read()
 
+# replace the actual loop names into the config template and create the device
+t1 = template.replace(loop1_template, loop1)
+t2 = t1.replace(loop2_template, loop2)
+print(f'template = {t2}')
 
-loop1=loop1.replace('\n','')
-loop2=loop2.replace('\n','')
-template = template.replace(loop1_template,loop1)
-templete = template.replace(loop2_template,loop2)
-print(f'template = {template}')
+# For some reson the following did hot do the second substitution
+#
+# template = template.replace(loop1_template, loop1)
+# template = template.replace(loop2_template, loop2)
 
-with open(def_file_name,'w') as of:
-    of.write(template)
+
+with open(def_file_name, 'w') as of:
+    of.write(t2)
 
 rc = subprocess.check_output(['dmsetup', 'create', target_device_name, def_file_name])
 print(rc)
